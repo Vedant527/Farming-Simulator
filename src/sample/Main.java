6b54e7ce7237619f1f3191f2162c16d8b3a3bef7
Vedant Bhat <vbhat34@gatech.edu>
2020-09-24 16:14:07 -0400

    Renames sample.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,39 +1,30 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @FXML
-    TextField NAME;
-    String name = NAME.getText();
 
-    @FXML ComboBox<String> DIFFICULTY;
-    @FXML ComboBox<String> SEASON;
-
-    ObservableList<String> difficulties = FXCollections
-            .observableArrayList("Easy", "Medium", "Hard");
-    ObservableList<String> seasons = FXCollections
-            .observableArrayList("Spring", "Summer", "Fall", "Winter");
+//    @FXML
+//    TextField NAME;
+//    String name = NAME.getText();
+//
+//    @FXML ComboBox<String> DIFFICULTY;
+//    @FXML ComboBox<String> SEASON;
+//    ObservableList<String> difficulties = FXCollections
+//            .observableArrayList("Easy", "Medium", "Hard");
+//    ObservableList<String> seasons = FXCollections
+//            .observableArrayList("Spring", "Summer", "Fall", "Winter");
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("CustomizePage.fxml"));
         primaryStage.setTitle("Farm Simulator");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
-
-        DIFFICULTY.setItems(difficulties);
-        SEASON.setItems(seasons);
-
 
     }
 
