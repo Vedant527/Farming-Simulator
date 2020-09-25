12552d12ad2f67303300ff8ab4fd158bc840599e
DavidCGordon <54375682+DavidCGordon@users.noreply.github.com>
2020-09-25 19:17:52 -0400

    first version farmUI page

--- src/FarmingSim/Main.java
+++ src/FarmingSim/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package FarmingSim;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("FarmUI.fxml"));
+        primaryStage.setTitle("FarmingSim");
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
