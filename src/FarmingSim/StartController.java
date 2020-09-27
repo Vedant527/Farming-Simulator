a7c9c8fea9fe63cdd02cf456337a82fea5a2c0d1
Vedant Bhat <vbhat34@gatech.edu>
2020-09-27 16:02:53 -0400

    adds graphics to start page

--- src/FarmingSim/StartController.java
+++ src/FarmingSim/StartController.java
@@ -1,9 +1,27 @@
 package FarmingSim;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 public class StartController {
+
+    @FXML Button BeginButton;
+
+    @FXML
+    private void initialize() {
+        Image img = new Image("FarmingSim/Resources/BeginButton.png");
+        ImageView startButton = new ImageView(img);
+        startButton.setFitWidth(120);
+        startButton.setFitHeight(40);
+
+        BeginButton.setGraphic(startButton);
+    }
+
     public void move_on(ActionEvent e) throws Exception {
         ScreenManager.addScreen(
                 "Player",
